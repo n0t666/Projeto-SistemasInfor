e8d736ebed262f5c86846df722b2345f6e952dc1
n0t666 <99896082+n0t666@users.noreply.github.com>
2024-11-11 03:01:57 +0000

    Adicionado userdata

--- playxchangewebapp/frontend/models/SignupForm.php
+++ playxchangewebapp/frontend/models/SignupForm.php
@@ -2,6 +2,7 @@
 
 namespace frontend\models;
 
+use common\models\Userdata;
 use Yii;
 use yii\base\Model;
 use common\models\User;
@@ -14,6 +15,8 @@ class SignupForm extends Model
     public $username;
     public $email;
     public $password;
+    public $nif;
+    public $nome;
 
 
     /**
@@ -24,17 +27,24 @@ class SignupForm extends Model
         return [
             ['username', 'trim'],
             ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'Este username já foi usado.'],
             ['username', 'string', 'min' => 2, 'max' => 255],
 
             ['email', 'trim'],
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'Este email já foi usado.'],
 
             ['password', 'required'],
             ['password', 'string', 'min' => Yii::$app->params['user.passwordMinLength']],
+
+            ['nif', 'string','min' => 9, 'max' => 9],
+            ['nif', 'unique', 'targetClass' => '\common\models\Userdata', 'message' => 'Este NIF já está associado a outra conta.'],
+
+            ['nome', 'required'],
+            ['nome', 'string', 'max' => 200],
+
         ];
     }
 
@@ -48,15 +58,40 @@ class SignupForm extends Model
         if (!$this->validate()) {
             return null;
         }
-        
-        $user = new User();
-        $user->username = $this->username;
-        $user->email = $this->email;
-        $user->setPassword($this->password);
-        $user->generateAuthKey();
-        $user->generateEmailVerificationToken();
+        $transaction = Yii::$app->db->beginTransaction();
 
-        return $user->save() && $this->sendEmail($user);
+        try {
+            $user = new User();
+            $user->username = $this->username;
+            $user->email = $this->email;
+            $user->setPassword($this->password);
+            $user->generateAuthKey();
+            $user->generateEmailVerificationToken();
+
+            if($user->save()){
+                $auth = Yii::$app->authManager;
+                $ClientRole = $auth->getRole('cliente');
+                $auth->assign($ClientRole, $user->id);
+                $userdata = new Userdata();
+                $userdata->user_id = $user->id;
+                $userdata->nif = $this->nif;
+                $userdata->nome = $this->nome;
+                if($userdata->save(false)){
+                    $transaction->commit();
+                    return $this->sendEmail($user);
+                }else{
+                    $transaction->rollBack();
+                }
+            }
+
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+
+
+
+        return null;
     }
 
     /**
--- playxchangewebapp/frontend/views/site/signup.php
+++ playxchangewebapp/frontend/views/site/signup.php
@@ -16,12 +16,28 @@ $img = Yii::getAlias('@web') . "/images/authForms/register2.jpg";
     <div class="row">
         <div class="col-lg-10 col-xl-9 mx-auto">
             <div class="card flex-row my-5 border-0 shadow rounded-3 overflow-hidden">
-                <div class="card-img-left d-none d-md-flex" style=" width: 45%;background: scroll center url('<?= $img ?>');background-size: cover;">
+                <div class="card-img-left d-none d-md-flex" style="width: 45%; background: scroll center url('<?= $img ?>'); background-size: cover;">
                 </div>
                 <div class="card-body p-4 p-sm-5">
                     <h5 class="card-title text-center mb-5 fw-light fs-5">Registar</h5>
 
                     <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+
+                    <div class="form-floating mb-3">
+                        <?= $form->field($model, 'nome', [
+                            'options' => ['class' => 'form-floating mb-3'],
+                            'template' => "{input}\n{label}\n{error}",
+                            'labelOptions' => ['for' => 'floatingNome'],
+                        ])->textInput(['id' => 'floatingNome', 'placeholder' => 'Nome Completo', 'required' => true, 'autocomplete' => 'off']) ?>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <?= $form->field($model, 'nif', [
+                            'options' => ['class' => 'form-floating mb-3'],
+                            'template' => "{input}\n{label}\n{error}",
+                            'labelOptions' => ['for' => 'floatingNif'],
+                        ])->textInput(['id' => 'floatingNif', 'placeholder' => 'NIF', 'required' => false, 'autocomplete' => 'off','type' => 'number']) ?>
+                    </div>
 
                     <div class="form-floating mb-3">
                         <?= $form->field($model, 'username', [
