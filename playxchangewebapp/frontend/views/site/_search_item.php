b3e51575aee80bd013de4b84b243ec2253c0bf1a
n0t666 <99896082+n0t666@users.noreply.github.com>
2025-01-01 20:23:13 +0000

    Fix pesquisa

--- playxchangewebapp/frontend/views/site/_search_item.php
+++ playxchangewebapp/frontend/views/site/_search_item.php
@@ -16,8 +16,11 @@ use yii\helpers\Url;
                     <a href="<?= Url::to(['jogo/view', 'id' => $model->id]) ?>" class="searchResult-link"><?= Html::encode($model->nome) ?></a>
                 </h5>
                 <p class="searchResult-description mb-0">
-                    <?= Html::encode(mb_strimwidth($model->descricao, 0, 250, '...')) //mb_strimwidth foi utilizado aqui para limitar os caracters da string
-                    // sendo os seguintes parametros: 1º - a string em questão, 2º - a posição para começar do caracter, 3º - a posicao do caracter que acaba e por fim o que deve aparacer quando atigir a posição máxima  ?>
+                    <?php if ($model->descricao && strlen($model->descricao) > 0) {
+                        //mb_strimwidth foi utilizado aqui para limitar os caracters da string
+                        // sendo os seguintes parametros: 1º - a string em questão, 2º - a posição para começar do caracter, 3º - a posicao do caracter que acaba e por fim o que deve aparacer quando atigir a posição máxima
+                       Html::encode(mb_strimwidth($model->descricao, 0, 250, '...'));
+                    }  ?>
                 </p>
             </div>
         </div>
@@ -31,7 +34,9 @@ use yii\helpers\Url;
                     <a href="<?= Url::to(['utilizador/profile', 'username' => $model->username]) ?>" class="searchResult-link"><?= Html::encode($model->username) ?></a>
                 </h5>
                 <p class="searchResult-biography mb-0">
-                    <?= Html::encode(mb_strimwidth($model->profile->biografia, 0, 250, '...')) ?>
+                    <?php if($model->profile->biografia && strlen($model->profile->biografia) > 0) {
+                        Html::encode(mb_strimwidth($model->profile->biografia, 0, 250, '...'));
+                    } ?>
                 </p>
             </div>
         </div>
