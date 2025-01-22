5ddda75816bd049e638565e00d443b28a171986b
Leandro Pereira <opereira3591@gmail.com>
2025-01-22 15:10:17 +0000

    teste

--- playxchangewebapp/backend/modules/api/controllers/JogoController.php
+++ playxchangewebapp/backend/modules/api/controllers/JogoController.php
@@ -235,7 +235,7 @@ class JogoController extends ActiveController
             }
         }
         return [
-            'jogomaiscaro' => $jogoMaisCaro,
+            'preco' => $jogoMaisCaro,
         ];
 
     }
